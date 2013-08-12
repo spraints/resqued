16f44a00b730c36f5dfdd7d6782b23522c55f323
Matt Burke <spraints@gmail.com>
2013-08-12 15:45:53 -0400

    arg, it doesn't work. :(

--- a.rb
+++ a.rb
@@ -1,23 +1,38 @@
 def main
   r,w = IO.pipe
   if fork
-    r.close
-    parent(w)
-    res = Process.waitpid2
-    p [$$, res]
-  else
+    trap(:CHLD) { wait_for_child('signal') ; exit }
+    p [:parent, $$]
     w.close
-    child(r)
+    reader(r)
+    wait_for_child('natural')
+  else
+    p [:child, $$]
+    #r.close
+    exec 'ruby', $0, 'child', r.to_i.to_s
+    writer(w)
   end
 end
 
-def parent(w)
-  w.puts "#$$ I AM PARENT"
+def wait_for_child(reason)
+  res = Process.waitpid2
+  p [$$, reason, res]
+end
+
+def writer(w)
+  w.puts "#$$ I AM THE WRITER"
   w.close
 end
 
-def child(r)
+def reader(r)
   puts "#$$ reads: #{r.readline}"
 end
 
-main
+if ARGV[0] == 'child'
+  fd = ARGV[1].to_i - 1
+  p [fd, f = IO.for_fd(ARGV[1].to_i, 'w')]
+  puts "I AM FAKE"
+  writer(f)
+else
+  main
+end
