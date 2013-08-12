18d9973169c6c895a906c20e7fd43979785feada
Matt Burke <spraints@gmail.com>
2013-08-12 16:31:34 -0400

    Use sockets to do IPC across an exec.

--- a.rb
+++ a.rb
@@ -10,25 +10,27 @@ def main
     wait_for_child('natural')
   else
     p [:child, $$]
-    #r.close
-    exec 'ruby', $0, 'child', r.to_i.to_s
-    writer(w)
+    r.close
+    exec 'ruby', $0, 'child', w.to_i.to_s
+    #writer(w)
   end
 end
 
 def wait_for_child(reason)
   res = Process.waitpid2
   p [$$, reason, res]
+rescue Errno::ECHILD => e
+  puts "[#$$] #{e} (#{reason})"
 end
 
 def writer(w)
-  w.send "#$$ I AM THE WRITER", 0
-  sleep 2
+  w.puts "#$$ I AM THE WRITER"
   w.close
+  sleep 2
 end
 
 def reader(r)
-  puts "#$$ reads: #{r.recv(100)}"
+  puts "#$$ reads: #{r.readline}"
 end
 
 if ARGV[0] == 'child'
