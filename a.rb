9162f7f75a5d5402bf090a183abfe47b3d5f5d1c
Matt Burke <spraints@gmail.com>
2013-08-12 16:53:53 -0400

    Use duplex sockets.

--- a.rb
+++ a.rb
@@ -1,36 +1,43 @@
 require 'socket'
 
 def main
-  r,w = Socket.socketpair(:UNIX, :DGRAM, 0)
+  #parent, child = Socket.socketpair(:UNIX, :DGRAM, 0)
+  parent, child = UNIXSocket.pair
   if fork
-    trap(:CHLD) { wait_for_child('signal') }
-    p [:parent, $$]
-    w.close
-    reader(r)
-    wait_for_child('natural')
+    puts "Control-\\ makes us talk. (Third time exits.)"
+    child.close
+    trap(:CHLD) { wait_for_child('signal', Process::WNOHANG) }
+    n = 0
+    trap(:QUIT) { parent.send("hi from #{$$}!", 0) ; n += 1 }
+    while n < 3
+      if IO.select([parent], nil, nil, 1.0)
+        puts parent.recv(100)
+      end
+    end
+    parent.send("exit", 0)
+    wait_for_child('normal')
   else
-    p [:child, $$]
-    r.close
-    exec 'ruby', $0, 'child', w.to_i.to_s
-    #writer(w)
+    parent.close
+    be_a_child(child)
   end
 end
 
-def wait_for_child(reason)
-  res = Process.waitpid2
+def be_a_child(child)
+  trap(:QUIT) { } # ^\ sends QUIT to all children of the foreground process. DUMB
+  loop do
+    if IO.select([child], nil, nil, 60.0)
+      s = child.recv(100)
+      return if s == 'exit'
+      child.send "you said, #{s.inspect}. (#$$)", 0
+    end
+  end
+end
+
+def wait_for_child(reason, waitpid_flags=0)
+  res = Process.waitpid2(waitpid_flags)
   p [$$, reason, res]
 rescue Errno::ECHILD => e
   puts "[#$$] #{e} (#{reason})"
-end
-
-def writer(w)
-  w.puts "#$$ I AM THE WRITER"
-  w.close
-  sleep 2
-end
-
-def reader(r)
-  puts "#$$ reads: #{r.readline}"
 end
 
 if ARGV[0] == 'child'
