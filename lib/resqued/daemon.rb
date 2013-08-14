db3f8a18a5c2a30f27dcefdfbe5f1d6ed89a72b9
Matt Burke <spraints@gmail.com>
2013-08-13 22:25:49 -0400

    No blocks!

--- lib/resqued/daemon.rb
+++ lib/resqued/daemon.rb
@@ -7,20 +7,24 @@ module Resqued
     # Public: daemonize and run the master process.
     def run
       rd, wr = IO.pipe
-      fork do
+      if fork
+        # grandparent
+        wr.close
+        begin
+          master_pid = rd.readpartial(16).to_i
+          exit
+        rescue EOFError
+          puts "Master process failed to start!"
+          exit! 1
+        end
+      elsif fork
         # parent
         Process.setsid
-        fork do
-          # master
-          @master.run(wr)
-        end
+        exit
+      else
+        # master
+        @master.run(wr)
       end
-      wr.close
-      master_pid = rd.readpartial(16).to_i
-      exit
-    rescue EOFError
-      puts "Master process failed to start!"
-      exit! 1
     end
   end
 end
