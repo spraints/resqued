7e87424d47ce66b842d90c3edd936aadcccb8a3d
Matt Burke <spraints@gmail.com>
2013-10-08 08:02:29 -0400

    Back to using `exe/resqued` to start a listener.

--- lib/resqued/listener.rb
+++ lib/resqued/listener.rb
@@ -30,8 +30,7 @@ module Resqued
       ENV['RESQUED_CONFIG_PATH'] = @config_paths.join(':')
       ENV['RESQUED_STATE']       = (@running_workers.map { |r| "#{r[:pid]}|#{r[:queue]}" }.join('||'))
       ENV['RESQUED_LISTENER_ID'] = @listener_id.to_s
-      listener = ENV['RESQUED_LISTENER_PATH'] || 'resqued-listener'
-      Kernel.exec(listener, socket_fd => socket_fd) # The hash at the end only works in new-ish (1.9+ or so) rubies. It's required for ruby 2.0.
+      Kernel.exec(Resqued::START_CTX['$0'], 'listener', socket_fd => socket_fd) # The hash at the end only works in new-ish (1.9+ or so) rubies. It's required for ruby 2.0.
     end
 
     # Public: Given args from #exec, start this listener.
