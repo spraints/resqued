558277ae347eac02d49398f98687fe9d117799cf
Matt Burke <spraints@gmail.com>
2013-10-14 12:05:31 -0400

    Don't start a new listener on SIGCONT.

--- lib/resqued/master.rb
+++ lib/resqued/master.rb
@@ -58,7 +58,7 @@ module Resqued
         when :CONT
           log "Resume job processing"
           @paused = false
-          kill_listener(:CONT)
+          kill_all_listeners(:CONT)
         when :INT, :TERM, :QUIT
           log "Shutting down..."
           kill_all_listeners(signal)
