2ce820e80bb8f0ff0dc695c80835a5129323f1b9
Matt Burke <spraints@gmail.com>
2013-10-17 07:55:30 -0400

    Test interrupted `yawn`.

---
+++ spec/resqued/sleepy_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require 'resqued/sleepy'
+require 'thread'
+
+describe Resqued::Sleepy do
+  include Resqued::Sleepy
+
+  it 'does not sleep if duration is 0'
+  it 'does not sleep if duration is negative'
+  it 'sleeps'
+
+  it 'wakes on `awake`' do
+    Thread.new { sleep 0.1 ; awake }
+    expect { yawn(2.0) }.to run_for(0.1).within(0.01)
+  end
+
+  it 'wakes on IO' do
+    rd, wr = IO.pipe
+    Thread.new { sleep 0.1 ; wr.write('.') }
+    expect { yawn(2.0, rd) }.to run_for(0.1).within(0.01)
+  end
+end
