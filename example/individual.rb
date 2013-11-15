14019621712615b934ac47a49f96586e71b15647
Matt Burke <spraints@gmail.com>
2013-11-15 07:06:44 -0500

    Even better, have a separate example config file for each style.

---
+++ example/individual.rb
@@ -0,0 +1,11 @@
+# Run like this:
+#
+#     $ resqued example/minimal.rb
+
+before_fork do
+  host = ENV['REDIS_HOST'] || 'localhost'
+  port = ENV['REDIS_PORT'] || 6379
+  Resque.redis = Redis.new(:host => host, :port => port)
+end
+
+worker 'resqued-example-queue'
