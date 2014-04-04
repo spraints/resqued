70d215218bb7f4dcd2bf144b09e23611e0e4b9e3
Matt Burke <spraints@gmail.com>
2014-04-04 09:20:12 -0400

    Ensure that require_relative is not overridden in required files.

--- spec/resqued/config_spec.rb
+++ spec/resqued/config_spec.rb
@@ -6,15 +6,18 @@ describe Resqued::Config do
   context do
     around do |example|
       $test_val = :not_set
+      $other_test_val = :not_set
       Dir.mktmpdir do |dir|
-        @config_file = make_file(dir, "config/resqued.rb", "require_relative '../lib/file.rb'\nworker 'example'\n")
-        make_file(dir, "lib/file.rb", "$test_val = :ok\n")
+        @config_file = make_file(dir, "config/resqued.rb", "require_relative '../lib/file'\nworker 'example'\n")
+        make_file(dir, "lib/file.rb", "require_relative 'file2'\n$test_val = :ok\n")
+        make_file(dir, "lib/file2.rb", "$other_test_val = :ok\n")
         example.call
       end
     end
     let(:config) { Resqued::Config.new([@config_file]) }
 
     it("can require_relative") { config.build_workers ; expect($test_val).to eq(:ok) }
+    it("does not override require_relative in required files") { config.build_workers ; expect($other_test_val).to eq(:ok) }
 
     def make_file(dir, relative_path, content)
       File.join(dir, relative_path).tap do |path|
