321ed3f86571e15075f609122bf824e6fb4d1d05
Matt Burke <spraints@gmail.com>
2015-03-19 15:16:41 -0400

    Make START_CTX aware of symlinked PWD.

--- lib/resqued.rb
+++ lib/resqued.rb
@@ -6,5 +6,18 @@ module Resqued
 
   def self.capture_start_ctx!
     START_CTX['$0'] = $0.dup
+    START_CTX['pwd'] =
+      begin
+        env_pwd = ENV["PWD"]
+        env_pwd_stat = File.stat env_pwd
+        dir_pwd_stat = File.stat Dir.pwd
+        if env_pwd_stat.ino == dir_pwd_stat.ino && env_pwd_stat.dev == dir_pwd_stat.dev
+          env_pwd
+        else
+          Dir.pwd
+        end
+      rescue
+        Dir.pwd
+      end
   end
 end
