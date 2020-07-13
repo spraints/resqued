298c6cea8ebfbd67847fb75d6638cdb227bdb039
Matt Burke <spraints@gmail.com>
2020-07-13 17:35:19 -0400

    Update rspec to 3.9.0

--- spec/resqued/backoff_spec.rb
+++ spec/resqued/backoff_spec.rb
@@ -6,7 +6,7 @@ describe Resqued::Backoff do
   let(:backoff) { described_class.new(min: 0.5, max: 64.0) }
 
   it "can start on the first try" do
-    expect(backoff.wait?).to be_false
+    expect(backoff.wait?).to be_falsey
   end
 
   it "has no waiting at first" do
@@ -15,42 +15,42 @@ describe Resqued::Backoff do
 
   context "after expected exits" do
     before { 3.times { backoff.started } }
-    it { expect(backoff.wait?).to be_true }
+    it { expect(backoff.wait?).to be true }
     it { expect(backoff.how_long?).to be_close_to(0.5) }
   end
 
   context "after one quick exit" do
     before { 1.times { backoff.started; backoff.died } }
-    it { expect(backoff.wait?).to be_true }
+    it { expect(backoff.wait?).to be true }
     it { expect(backoff.how_long?).to be_close_to(1.0) }
   end
 
   context "after two quick starts" do
     before { 2.times { backoff.started; backoff.died } }
-    it { expect(backoff.wait?).to be_true }
+    it { expect(backoff.wait?).to be true }
     it { expect(backoff.how_long?).to be_close_to(2.0) }
   end
 
   context "after five quick starts" do
     before { 6.times { backoff.started; backoff.died } }
-    it { expect(backoff.wait?).to be_true }
+    it { expect(backoff.wait?).to be true }
     it { expect(backoff.how_long?).to be_close_to(32.0) }
   end
 
   context "after six quick starts" do
     before { 7.times { backoff.started; backoff.died } }
-    it { expect(backoff.wait?).to be_true }
+    it { expect(backoff.wait?).to be true }
     it { expect(backoff.how_long?).to be_close_to(64.0) }
   end
 
   context "does not wait longer than 64s" do
     before { 8.times { backoff.started; backoff.died } }
-    it { expect(backoff.wait?).to be_true }
+    it { expect(backoff.wait?).to be true }
     it { expect(backoff.how_long?).to be_close_to(64.0) }
     it "and resets after an expected exit" do
       backoff.started
       backoff.started
-      expect(backoff.wait?).to be_true
+      expect(backoff.wait?).to be true
       expect(backoff.how_long?).to be_close_to(0.5)
     end
   end
