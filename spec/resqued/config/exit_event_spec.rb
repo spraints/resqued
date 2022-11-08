38ec04b5125e981ad1a19ee773ef5f50c4e1b83b
Eric Sun <erichhsun@github.com>
2022-11-08 10:46:24 -0800

    Update spec/resqued/config/exit_event_spec.rb
    Co-authored-by: Matt Burke <spraints@users.noreply.github.com>

--- spec/resqued/config/exit_event_spec.rb
+++ spec/resqued/config/exit_event_spec.rb
@@ -30,7 +30,7 @@ describe do
       # In ruby versions < 3, Process::Status evaluates $? regardless of what status is returned by
       # exitcode for a status created with Process::Status.allocate
       fork do
-        0
+        exit!
       end
       status = Process.waitpid2[1]
       Resqued::Config::AfterExit.new(worker_summary: Resqued::WorkerSummary.new(alive_time_sec: 1, process_status: status))
